64dd1b006469b006aebb8079b45b8529911886f9
jorgenbs <jorgenbs@gmail.com>
2013-10-10 11:04:27 +0200

    dynamic widget width size

--- BouvetOneRegistation/App/viewmodels/program.js
+++ BouvetOneRegistation/App/viewmodels/program.js
@@ -2,8 +2,6 @@
     
     var program = ko.observableArray([]),
         rooms = ko.observableArray([]),
-        col_width = 200,
-        row_height = 45,
         prev_grid_position = 0;
         
     var columnClass = ko.computed(function() {
@@ -14,6 +12,8 @@
     var addWidget = function (session, booking, timeslotIndex) {
         var el = $("<li></li>").text(session.title)
             .addClass('widget-not-draggable')
+            .attr('data-sizex', 1)
+            .attr('data-sizey', 1)
             .attr('data-session-id', session.id);
 
         if (booking != null) {
@@ -26,45 +26,48 @@
         return el;
     };
     
-    var activate = function() {
-        programService.getDayWithTimeSlots(1).then(function (day) {
-            programService.fillBookingsForDay(day).done(function () {
-                programService.fillEmbeddedInfo(day).done(function () {
+    var activate = function () {
+        //get rooms for given day
+        programService.getRoomsAsync(1).then(function (_rooms) {
+            rooms(_.sortBy(_rooms, function (room) { return room.slotIndex; }));
+            
+            programService.getDayWithTimeSlots(1).then(function (day) {
+                programService.fillBookingsForDay(day).done(function () {
+                    programService.fillEmbeddedInfo(day).done(function () {
 
-                    program(_.map(day.timeslots, function (timeslot) {
-                        return {
-                            id: timeslot.id,
-                            displayTime: moment(timeslot.startTime).format('HH:mm') + '-' + moment(timeslot.endTime).format('HH:mm'),
-                            bookings: timeslot.bookings
-                        };
-                    }));
-                    
-                    //initialize gridster
-                    gridster = $('.gridster ul').gridster({
-                        widget_margins: [5, 5],
-                        widget_base_dimensions: [col_width, row_height],
-                        avoid_overlapped_widgets: true,
-                        max_cols: rooms().length + 1,
-                        max_rows: rooms().length + 1,
-                        static_class: 'widget-not-draggable',
-                        draggable: {
-                            items: ".gs_w:not(.widget-not-draggable)"
-                        }
-                    }).data('gridster');
-                    
-                    //add booked sessions to gridster
-                    _.each(program(), function (timeslot, timeslotIndex) {
-                        _.each(timeslot.bookings, function (booking) {
-                            addWidget(booking.session, booking, timeslotIndex);
+                        program(_.map(day.timeslots, function (timeslot) {
+                            return {
+                                id: timeslot.id,
+                                displayTime: moment(timeslot.startTime).format('HH:mm') + '-' + moment(timeslot.endTime).format('HH:mm'),
+                                bookings: timeslot.bookings
+                            };
+                        }));
+
+                        var g_el = $('.gridster ul');
+                        var w_width = (g_el.width() / (rooms().length + 1)) - 10;
+
+                        //initialize gridster
+                        gridster = g_el.gridster({
+                            widget_margins: [5, 5],
+                            widget_base_dimensions: [w_width, 45],
+                            avoid_overlapped_widgets: true,
+                            max_cols: rooms().length + 1,
+                            max_rows: rooms().length + 1,
+                            static_class: 'widget-not-draggable',
+                            draggable: {
+                                items: ".gs_w:not(.widget-not-draggable)"
+                            }
+                        }).data('gridster');
+
+                        //add booked sessions to gridster
+                        _.each(program(), function (timeslot, timeslotIndex) {
+                            _.each(timeslot.bookings, function (booking) {
+                                addWidget(booking.session, booking, timeslotIndex);
+                            });
                         });
                     });
                 });
             });
-        });
-
-        //get rooms for given day
-        programService.getRoomsAsync(1).then(function(rooms) {
-            rooms(_.sortBy(rooms, function (room) { return room.slotIndex; }));
         });
     };
 
